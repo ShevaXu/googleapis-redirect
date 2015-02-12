009210f4e3b9aaa33048173c2c8a84818a51f840
Sheva Xu <shevaxu@outlook.com>
2015-02-12 14:55:43 +0800

    Make it general

--- redirect.js
+++ redirect.js
@@ -2,11 +2,17 @@ console.log("it's on");
 
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
-		//console.log(details.url);
-		if( details.url == "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js" ) {
-			console.log(details.url);
-			return { redirectUrl: "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js" };
+		var newUrl = '';
+		switch (details.url) {
+			// urls that are not exact match
+			case "https://ajax.googleapis.com/ajax/libs/angularjs/1.2.19/angular.min.js":
+				newUrl = "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.19/angular.min.js";	// angularjs -> angular.js
+				break;
+			default:
+				newUrl = "https://cdnjs.cloudflare.com" + details.url.substring(details.url.indexOf(".com") + 4);
 		}
+		console.log("Switch URL to " + newUrl);
+		return { redirectUrl: newUrl };
 	},
 	{urls: [
 		"*://ajax.googleapis.com/*"
