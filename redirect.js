191235f191f01d1b1352efe972149e4af5773303
Sheva Xu <shevaxu@outlook.com>
2017-02-06 19:49:03 +0800

    Add google-analytics.com

--- redirect.js
+++ redirect.js
@@ -46,6 +46,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     return { cancel: true }
   },
   {urls: [
+    '*://*.google-analytics.com/*',
     '*://fonts.googleapis.com/*'
   ]},
   ['blocking']
