3afa86a4171dd451c98f87307074128e4add8546
Sheva Xu <shevaxu@outlook.com>
2017-03-03 15:05:53 +0800

    Add options

--- redirect.js
+++ redirect.js
@@ -1,53 +1,63 @@
 console.log("it's on")
 
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    var oldUrl = details.url
-    // parse the old one
-    var mark = 'libs/'
-    var target = oldUrl.substring(oldUrl.indexOf(mark) + mark.length)
-    console.log(target)
-    var parts = target.split('/')
-    var lib = parts[0]
-    var ver = parts[1]
-    var file = parts[2]
-    console.log(parts)
-    // bad cases for lib
-    var newLib = ''
-    switch (lib) {
-      case 'angularjs':
-        newLib = 'angular.js'
-        break
-      default:
-        newLib = lib
-    }
-    // bad cases for file
-    var newFile = ''
-    switch (file) {
-      case 'webfont.js':
-        newFile = 'webfontloader.js'
-        break
-      default:
-        newFile = file
-    }
-    var newUrl = 'https://cdnjs.cloudflare.com/ajax/libs/' + newLib + '/' + ver + '/' + newFile
-    console.log('Switch URL to ' + newUrl)
-    return { redirectUrl: newUrl }
-  },
-  {urls: [
-    '*://ajax.googleapis.com/*'
-  ]},
-  ['blocking']
-)
+// global options
+var hasVPN = true
+var cancelNonessential = true
 
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    console.log('Requesting', details.url)
-    return { cancel: true }
-  },
-  {urls: [
-    '*://*.google-analytics.com/*',
-    '*://fonts.googleapis.com/*'
-  ]},
-  ['blocking']
-)
+// the RULE below is unstable, use it only if you do not have good VPN
+if (!hasVPN) {
+  chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+      var oldUrl = details.url
+      // parse the old one
+      var mark = 'libs/'
+      var target = oldUrl.substring(oldUrl.indexOf(mark) + mark.length)
+      console.log(target)
+      var parts = target.split('/')
+      var lib = parts[0]
+      var ver = parts[1]
+      var file = parts[2]
+      console.log(parts)
+      // bad cases for lib
+      var newLib = ''
+      switch (lib) {
+        case 'angularjs':
+          newLib = 'angular.js'
+          break
+        default:
+          newLib = lib
+      }
+      // bad cases for file
+      var newFile = ''
+      switch (file) {
+        case 'webfont.js':
+          newFile = 'webfontloader.js'
+          break
+        default:
+          newFile = file
+      }
+      var newUrl = 'https://cdnjs.cloudflare.com/ajax/libs/' + newLib + '/' + ver + '/' + newFile
+      console.log('Switch URL to ' + newUrl)
+      return { redirectUrl: newUrl }
+    },
+    {urls: [
+      '*://ajax.googleapis.com/*'
+    ]},
+    ['blocking']
+  )
+}
+
+// fonts & analytics are usually not essential for users
+if (cancelNonessential) {
+  chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+      console.log('Requesting', details.url)
+      return { cancel: true }
+    },
+    {urls: [
+      '*://*.google-analytics.com/*',
+      '*://fonts.googleapis.com/*'
+    ]},
+    ['blocking']
+  )
+}
